602e754b9241eb3178a54d6229080a4c6cb9830b
jantiks <58886410+jantiks@users.noreply.github.com>
2020-12-20 21:05:27 +0400

    Initial Commit

---
+++ passportPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  passportPhoto
+//
+//  Created by Tigran on 12/20/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
