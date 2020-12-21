4d9505347bebf16dc5050035e1be475017c56776
jantiks <58886410+jantiks@users.noreply.github.com>
2020-12-21 17:45:56 +0400

    configured download and crop again buttons , added functions to the same buttons , and modified logic of working tabbarcontroller

--- passportPhoto/ExportController.swift
+++ passportPhoto/ExportController.swift
@@ -10,27 +10,55 @@ import UIKit
 
 class ExportController: UIViewController {
 
+    @IBOutlet var cropAgainButton: UIButton!
+    @IBOutlet var downloadButton: UIButton!
     var croppedImage: UIImage?
     
     
     @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        if let image = croppedImage {
-//            imageView.image = image
-//        }
         
+        
+        cropAgainButton.layer.borderWidth = 3
+        cropAgainButton.layer.cornerRadius = 5
+        cropAgainButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        downloadButton.layer.borderWidth = 3
+        downloadButton.layer.cornerRadius = 5
+        downloadButton.layer.borderColor = UIColor.systemBlue.cgColor
     
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         if let image = croppedImage {
             imageView.image = image
         }
     }
     
-
     
+    @IBAction func downloadTapped(_ sender: Any) {
+        
+        guard let image = imageView.image else { return }
+        
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @IBAction func cropAgainTapped(_ sender: Any) {
+        tabBarController?.tabBar.items?[1].isEnabled = false
+        tabBarController?.selectedIndex = 0
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        
+        if let error = error {
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Image has beed save to your photos", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(ac, animated: true)
+        }
+    }
 
 }
